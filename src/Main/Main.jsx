708fe04c674ae65dd1efd6c35f778b5059d36ab8
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-08-17 15:16:30 +0600

    deploy

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
         {
 path:"/",
 element:<Home/>,
-loader:()=>fetch(`${import.meta.env.VITE_API_URL}/products`)
+loader:()=>fetch(`https://scic-server-kappa.vercel.app/products`)
         }
      
       ]
